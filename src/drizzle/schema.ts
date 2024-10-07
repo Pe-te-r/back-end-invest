a5d2ce2352c53e3323c27bffcfe9cdcc231ab4a5
Pe-te-r <mburupeter8526@gmail.com>
2024-10-07 17:32:03 +0300

    schema info halfway

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,32 +1,68 @@
 import { relations } from "drizzle-orm";
-import { pgTable, varchar,date, boolean,  uuid } from "drizzle-orm/pg-core";
+import { pgTable, integer, varchar, date, boolean, uuid } from "drizzle-orm/pg-core";
 
-export const usersTable=pgTable("users",{
+export const usersTable = pgTable("users", {
     id: uuid('id').defaultRandom().primaryKey(),
-    first_name: varchar('first_name',{length:20}),
-    last_name: varchar('last_name',{length:20}),
-    email: varchar('email',{length:100}),
+    first_name: varchar('first_name', { length: 20 }),
+    last_name: varchar('last_name', { length: 20 }),
+    email: varchar('email', { length: 100 }),
     verified: boolean('verified').default(false),
+    invested: boolean('verified').default(false),
+    promo_code_owner: varchar('promo_code_owner'),
     created_at: date('created_at').defaultNow(),
+});
+
+export const authTable = pgTable('auth_pass', {
+    user_id: uuid('user_id').references(() => usersTable.id).primaryKey(),
+    password: varchar('password', { length: 100 }),
+    created_at: date('created_at').defaultNow(),
+});
+
+export const accountTable = pgTable('account', {
+    user_id: uuid('user_id').references(() => usersTable.id).primaryKey(),
+    balance: integer('balance').default(0),    
+});
+
+export const server = pgTable('server',{
+    id: uuid('id').defaultRandom().primaryKey(),
+    money: varchar('name', { length: 50 }),
 })
 
-export const  authTable = pgTable('auth_pass',{
-    user_id: uuid('user_id').references(()=> usersTable.id),
-    password: varchar('password',{length:100}),
-    created_at: date('created_at').defaultNow(),
+export const promoCode = pgTable('promoCode',{
+    user_id: uuid('user_id').references(() => usersTable.id).primaryKey(),
+    promo_code: varchar('promo_code', { length: 20 }),
 })
 
+export const promoUsers = pgTable('promoUsers',{
+    user_id: uuid('user_id').references(() => usersTable.id).primaryKey(),
+    
+})
 
-
-// relationship
+// relationships
 
 export const usersRelations = relations(usersTable, ({ one }) => ({
     auth: one(authTable),
-  }));
+    account: one(accountTable),
+    promoCode: one(promoCode),
+}));
 
 export const authRelations = relations(authTable, ({ one }) => ({
     user: one(usersTable, {
-      fields: [authTable.user_id],
-      references: [usersTable.id],
+        fields: [authTable.user_id],
+        references: [usersTable.id],
     }),
-  }));
+}));
+
+export const promoRelations = relations(promoCode,({ one }) => ({
+    user: one(usersTable, {
+        fields: [promoCode.user_id],
+        references: [usersTable.id],
+    }),
+}));
+
+export const accountRelations = relations(accountTable, ({ one }) => ({
+    user: one(usersTable, {
+        fields: [accountTable.user_id],
+        references: [usersTable.id],
+    }),
+}));
